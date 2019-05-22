6327ea14d6dab9697451bf285be689b405148636
patpalmerston <patpalmerston@gmail.com>
2019-05-22 13:57:54 -0700

    first few tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,15 @@
-// Test away!
+import React from 'react';
+
+import { render, fireEvent  } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each'
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+import Controls from './../controls/Controls';
+
+describe('Controls Module', () => {
+
+  it('should render Controls Module', () => {
+    render(<Controls />);
+  })
+  
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,22 @@
-// Test away
+import React from 'react';
+import ReactDOm from 'react-dom';
+import { render, fireEvent  } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './../controls/Controls';
+import Display from './../display/Display';
+
+describe('dashboard component', () => {
+
+  it('check close button call', () => {
+    const mockFn = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={mockFn} />)
+
+    const closeButton = getByText('Close Gate');
+
+    fireEvent.click(closeButton);
+
+    expect(mockFn).toHaveBeenCalled();
+  })
+
+
+})
